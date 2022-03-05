9c229a300b846a98870c1bc83ff6d38cfb610ae0
So Nakamura <sonakamura.aoichi@gmail.com>
2022-03-05 20:41:06 +0900

    NavigationBarの上を接続

--- Ikuseiron/ProfileViewController.swift
+++ Ikuseiron/ProfileViewController.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UINavigationBarDelegate {
 
     @IBOutlet var ikuseiView: UIView!
     @IBOutlet var ikuseiTabView: UIView!
+    
+    @IBOutlet weak var selfIkuseiron: UIButton!
+    @IBOutlet weak var bookmarkIkuseiron: UIButton!
+    
+    @IBOutlet weak var profileNavigationBar: UINavigationBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +32,24 @@ class ProfileViewController: UIViewController {
         
         ikuseiTabView.layer.cornerRadius = 10
         ikuseiTabView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        // ボタンの外枠の色を変更
+        selfIkuseiron.layer.borderColor = UIColor.gray.cgColor
+        bookmarkIkuseiron.layer.borderColor = UIColor.gray.cgColor
+        // ボタンの外枠の太さを変更
+        selfIkuseiron.layer.borderWidth = 1.0
+        bookmarkIkuseiron.layer.borderWidth = 1.0
+        // ボタンの角を丸くしている
+        selfIkuseiron.layer.cornerRadius = 15
+        bookmarkIkuseiron.layer.cornerRadius = 15
+        
+        // NavigationBarの設定
+        profileNavigationBar.delegate = self
+    }
+    
+    // これによってNavigationBarを上につなげてる
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
     }
     
 
